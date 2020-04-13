2251d06cecff71c82df839c763cd272ec964874a
Roman-Orlov <61241051+Roman-Orlov@users.noreply.github.com>
2020-04-13 19:58:20 +0300

    Update rds_module_1.py

---
+++ module_1/rds_module_1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+from collections import Counter
+data['profit'] = data['revenue'] - data['budget']
+year_08 = data[data.release_year==2008]
+year_08[data.profit == data.profit.max()]
