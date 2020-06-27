8a6329ca551571b29d4302f4c0b3890e223cd6c8
Roman-Orlov <61241051+Roman-Orlov@users.noreply.github.com>
2020-06-27 17:07:33 +0300

    Update RDS_Module2.py

--- module_2/RDS_Module2.py
+++ module_2/RDS_Module2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -15,17 +9,9 @@ pd.set_option('display.max_columns', 50)  # показывать больше к
 
 students = pd.read_csv('stud_math.csv')
 
-
-# In[19]:
-
-
 # Первичный отсмотр данных
 display(students.head(5))
 students.info()
-
-
-# In[4]:
-
 
 # анализируем числовые данные
 def get_stat_number(column, n):
@@ -41,7 +27,6 @@ def get_stat_number(column, n):
                                                                                                                label='Выбросы')
     plt.legend()
 
-
 # функция удаления выбросов
 def del_outlier(column):
     median = students[column].median()
@@ -50,50 +35,25 @@ def del_outlier(column):
     perc75 = students[column].quantile(0.75)
     return students.loc[students[column].between(perc25 - 1.5*IQR, perc75 + 1.5*IQR)]
 
-
-# In[5]:
-
-
 # столбец age
 get_stat_number('age',8)
-
-
-# In[6]:
-
 
 # уберем выбросы
 students = del_outlier('age')
 
-
-
 # столбец studytime, granular
 get_stat_number('studytime, granular',4)
-
-
-# In[8]:
-
 
 # уберем выбросы
 students = del_outlier('studytime, granular')
 
-
-# In[128]:
-
-
 # столбец score
 get_stat_number('score',10)
-
-
-# In[129]:
-
 
 # уберем выбросы
 students = del_outlier('score')
 
-
-# In[130]:
-
-
+# функция перевода текстовоый формат
 def transform_number_to_text(x):
     return str(x)
 
@@ -101,17 +61,9 @@ def transform_number_to_text(x):
 for c in ('Medu', 'Fedu', 'traveltime', 'studytime', 'freetime', 'goout', 'health'):
     students[c]=students[c].apply(transform_number_to_text)
 
-
-# In[131]:
-
-
 # посмотрим на категориальные данные
 for c in ('school', 'sex', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic','Medu', 'Fedu', 'traveltime', 'studytime', 'freetime', 'goout', 'health'):
     print(display(students[c].value_counts(dropna=False)), students.loc[:, [c]].info())
-
-
-# In[139]:
-
 
 def get_stat_dif(column):
     cols = students.loc[:, column].value_counts().index[:10]
@@ -123,13 +75,8 @@ def get_stat_dif(column):
             print('Найдены статистически значимые различия для колонки', column)
             break
 
-
 for col in ['school', 'sex', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic','Medu', 'Fedu', 'traveltime', 'studytime', 'freetime', 'goout', 'health']:
     print(get_stat_dif(col))
-
-
-# In[133]:
-
 
 fig, ax = plt.subplots(figsize=(14, 4))
 sns.boxplot(x='address', y='score',
@@ -156,33 +103,8 @@ for col in ['Medu', 'Fedu', 'studytime', 'goout', 'sex', 'address', 'Fjob', 'sch
     get_boxplot(col)
 
 
-# In[134]:
-
-
-#students = students.drop(['Medu', 'Fedu', 'traveltime', 'studytime', 'freetime', 'goout', 'health', 'school', 'sex', 'address', 'famsize',
-#                          'Pstatus', 'Fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic'], axis=1)
-
-
-# In[135]:
-
-
-students
-
-
-# In[136]:
-
-
 students.corr()
 
 
-# In[137]:
-
-
 sns.pairplot(students, kind = 'reg')
-
-
-# In[ ]:
-
-
-
 
